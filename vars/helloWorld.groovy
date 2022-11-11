e99c92c5445a45be1f4ba5e6745abdf31eaf0c47
Gurnish Kohli <kohligurnish5@gmail.com>
2022-11-11 14:24:31 +0530

    Calling constants

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,7 @@
- def call(String name, String surName){
-    def helloWorld = "Hello!! greetings $name $surName"
-    return helloWorld
- }
+import ace.jenkins.Costants.groovy
+
+def call(String name, String surName){
+   def helloWorld = "Hello!! greetings $name $surName"
+   echo "$MAJOR_VERSION"
+   return helloWorld
+}
